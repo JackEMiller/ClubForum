3b6bfd0d9abd7afd5dbe155b9a88dcf8269e4bea
jmiller2612 <jmiller2612@gmail.com>
2021-03-17 10:13:02 +0000

    changed classes to init_app, added create_app function

--- application/init_app.py
+++ application/init_app.py
@@ -1,6 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    return app
 
 
 class Users(db.Model):
