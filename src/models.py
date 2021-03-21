9e9d2d660ee50d0d1edc3e62b593065eb18fe034
JackEMiller <79214097+JackEMiller@users.noreply.github.com>
2021-03-21 19:52:45 +0000

    Update models.py

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@34.89.4.172/db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     db.create_all(app=app)
